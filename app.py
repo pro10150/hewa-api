b8d703e1b043332ff369c0fd8fa957357a749696
Noppakun Anantakitthawon <67267679+pro10150@users.noreply.github.com>
2022-05-03 00:58:26 +0700

    commit

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, jsonify, request
 import recommendation as rcm
 import base64
+import sqlite3
 
 app = Flask(__name__)
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+con = sqlite3.connect('Hewa.db', check_same_thread=False)
+con.row_factory = sqlite3.Row
+conList = []
+con.row_factory = dict_factory
+cur = con.cursor()
 
 
 @app.route('/recommendation', methods=['POST'])
@@ -30,6 +42,17 @@ def get_recommendation():
     print(type(data))
     return jsonify
 
+@app.route('/query', methods=['GET'])
+def comment_readlDataFromSQLite():
+    data = request.args.get('query')
+    print(data)
+    cur.execute(data)
+    # print(cur.fetchone())
+    # for row in cur.execute('SELECT * FROM menuTABLE'):
+    #     conList.append(row)
+    #     print(row)
+    return jsonify(cur.fetchall())
+
 
 if __name__ == "__main__":
-    app.run(threaded=True, port=6000)
+    app.run(host='0.0.0.0', threaded=True, port=6000, debug=True)
